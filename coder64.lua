1988d2be5c71b17b1327457ea197b17e4afc78bd
viskontin <116100387+viskontin@users.noreply.github.com>
2022-10-18 18:58:22 +0300

    Add files via upload

---
+++ coder64.lua
@@ -0,0 +1,15 @@
+print("Lua fork version of Coder!")
+print("Enter your normal number")
+acl = io.read("n")
+
+-- никому не нужные ключи
+keys1 = "0.9485988"
+keys2 = "1.4938478"
+keys3 = "2.4398430"
+keys4 = "0.0069898"
+keys5 = "0.7950348"
+keys6 = "5.3904305"
+
+
+answ = acl * keys2 / keys1 * keys3 / keys4 * keys5 / keys6
+print("Your fndr64 number:", answ)
